1f479624231a56f24ec1b24802edb83510178ae1
Joascode <joaskramer@outlook.com>
2018-03-08 20:25:58 +0100

    First Commit

---
+++ ProxyApp/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ProxyApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public ObservableCollection<string> RequestsList { get; set; }
+
+        public MainWindow()
+        {
+            RequestsList = new ObservableCollection<string>();
+            InitializeComponent();
+            DataContext = this;
+            //RequestsList.AddRequest("Listening");
+            
+        }
+
+        private void StartBtn_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Starting the server.");
+            RequestsList.Add("Listening for requests.");
+        }
+    }
+}
