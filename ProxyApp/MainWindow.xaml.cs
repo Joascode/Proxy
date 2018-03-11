286b170f0e2549caf5acbf348b2a83c8787dcca5
Joascode <joaskramer@outlook.com>
2018-03-11 17:47:11 +0100

    Revamped alot of the code. Made callbacks for UI changes so the UI is mostly gone from the RequestHandler. Added Request/Response filtering and fixed bugs for missing headers. Working on image filtering.

--- ProxyApp/MainWindow.xaml.cs
+++ ProxyApp/MainWindow.xaml.cs
@@ -21,24 +21,122 @@ namespace ProxyApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        private RequestHandler RequestHandler;
+        private RequestHandler requestHandler;
         public ObservableCollection<string> RequestsList { get; set; }
-        //public ObservableCollection<Request> Requests { get; set; }
+        private int port = 8080;
+        private int buffer = 1024;
+        private bool listening = false;
+        private bool filterRequest = false;
+        private bool filterResponse = false;
 
         public MainWindow()
         {
             RequestsList = new ObservableCollection<string>();
             InitializeComponent();
             DataContext = this;
-            //RequestsList.AddRequest("Listening");
         }
 
         private void StartBtn_Click(object sender, RoutedEventArgs e)
         {
-            RequestHandler = new RequestHandler(this);
-            //Console.WriteLine("Starting the server.");
-            RequestHandler.Listen();
-            AddToLog("Listening for requests.");
+            if(listening)
+            {
+                listening = false;
+                requestHandler.Close();
+                requestHandler = null;
+                AddToLog("Closing the listener.");
+            } else
+            {
+                listening = true;
+                requestHandler = new RequestHandler(this, port, buffer);
+                requestHandler.Listen((string message, RequestHandler.Types reponseType) =>
+                {
+                    switch(reponseType)
+                    {
+                        case RequestHandler.Types.request:
+                        {
+                            if (filterRequest) break;
+                            else
+                            {
+                                AddToLog(message);
+                                break;
+                            }
+                        }
+                        case RequestHandler.Types.response:
+                        {
+                            if (filterResponse) break;
+                            else
+                            {
+                                AddToLog(message);
+                                break;
+                            }
+                        }
+                    }
+                });
+                AddToLog("Listening for requests.");
+            }
+            
+            ListenBtn.Content = listening ? "Stop" : "Start";
+        }
+
+        private void PortChangedHandler(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                if (int.TryParse(PortNumberTxt.Text, out port))
+                {
+                    if(port <= 0)
+                    {
+                        AddToLog("Please enter a positive port number.");
+                    } else
+                    {
+                        if (requestHandler != null) AddToLog("Can't change port while listening.");
+                        else AddToLog($"Changed port to: {port}");
+                    }
+                }
+            }
+        }
+
+        private void BufferSizeChangedHandler(object sender, KeyEventArgs e)
+        {
+            if(e.Key == Key.Return)
+            {
+                if (int.TryParse(BufferSizeTxt.Text, out buffer))
+                {
+                    if (buffer <= 0)
+                    {
+                        AddToLog("Please enter a positive buffer size.");
+                    }
+                    else
+                    {
+                        if (requestHandler != null)
+                        {
+                            requestHandler.BufferSize = buffer;
+                        }
+                        AddToLog($"Changed buffer size to: {buffer}");
+                    }
+                }
+            } 
+        }
+
+        private void ClearLogBtn_Click(object sender, RoutedEventArgs e)
+        {
+            RequestsList.Clear();
+        }
+
+        private void ContentInCheck_Click(object sender, RoutedEventArgs e)
+        {
+            filterRequest = (bool) ContentInCheckBox.IsChecked;
+        }
+
+        private void ContentUitCheck_Click(object sender, RoutedEventArgs e)
+        {
+            filterResponse = (bool)ContentUitCheckBox.IsChecked;
+        }
+
+        private void HeaderEditCheck_Click(object sender, RoutedEventArgs e)
+        {
+            //Catch error when proxy isn't listening yet.
+            requestHandler.RemoveBrowser = (bool)HeaderEditCheckBox.IsChecked;
         }
 
         public void AddToLog(string request)
--- ProxyApp/RequestHandler.cs
+++ ProxyApp/RequestHandler.cs
@@ -4,7 +4,9 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Net.Sockets;
+using System.Security;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ProxyApp
@@ -14,88 +16,287 @@ namespace ProxyApp
         MainWindow window;
         TcpListener listener;
         TcpClient client;
-        byte[] buffer = new byte[1024];
+        NetworkStream ns;
+        private StringBuilder sb = new StringBuilder();
+        private static readonly string[] _imageExtensions = { "jpg", "bmp", "gif", "png" };
+        
+        private Action<string, Types> callback2;
+        public enum Types
+        {
+            request,
+            response
+        }
 
-        public RequestHandler(MainWindow window)
+        private bool removeBrowser;
+        public bool RemoveBrowser
+        {
+            get
+            {
+                return removeBrowser;
+            }
+            set
+            {
+                removeBrowser = value;
+            }
+        }
+
+        private int bufferSize;
+        public int BufferSize
+        {
+            get
+            {
+                return bufferSize;
+            }
+            set
+            {
+                bufferSize = value;
+                buffer = new byte[bufferSize];
+            }
+        }
+        private byte[] buffer;
+
+        private int port;
+        public int Port
+        {
+            get
+            {
+                return port;
+            }
+            set
+            {
+                port = value;
+            }
+        }
+
+        public RequestHandler(MainWindow window, int port, int bufferSize)
         {
             this.window = window;
+            Port = port;
+            BufferSize = bufferSize;
+            buffer = new byte[bufferSize];
         }
 
-        public async void Listen()
+        public async void Listen(Action<string, Types> callback)
         {
-            listener = new TcpListener(IPAddress.Any, 8080);
+            listener = new TcpListener(IPAddress.Any, port);
             listener.Start();
-
-            
-            Console.WriteLine("Client found.");
-
-            while(true)
+            callback2 = callback;
+            window.AddToLog($"Listening on port: {port}");
+            while (true)
             {
-                client = await listener.AcceptTcpClientAsync();
-                await Task.Run(() => HandleMessages());
+                try
+                {
+                    client = await listener.AcceptTcpClientAsync();
+                    await Task.Run(() => HandleMessages(callback));
+                } catch(InvalidOperationException)
+                {
+                    //Geen nette manier van sluiten, maar nu geen tijd voor.
+                    break;
+                }
+                    
             }
-            //Convert client info to request info
-            
         }
 
-        private void HandleMessages()
+        public void Close()
         {
-            string completeMessage = "";
-            do
+            listener.Stop();
+        }
+
+        private void HandleMessages(Action<string, Types> callback)
+        {
+            using (ns = client.GetStream())
             {
-                client.GetStream().Read(buffer, 0, buffer.Length);
-                string message = Encoding.ASCII.GetString(buffer, 0, buffer.Length);
-                completeMessage += message;
-            } while (client.GetStream().DataAvailable);
+                string message = ReadMessage();
 
-            window.AddToLog(completeMessage);
+                if (removeBrowser) message = RemoveBrowserHeader(message);
 
+                callback(message, Types.request);
+                
+                string url = GetUrlFromRequest(message);
+                if(url != null)
+                {
+                    //Make this more pretty. RequestForImage only requires url so this can be done inside ReachSite.
+                    if (RequestForImage(message)) url = "file:///E:/ProgrammingProjects/WIN/ProxyApp/ProxyApp/images/placeholder.png";
+                    string[] response = ReachSite(url);
 
-            string url = GetUrlFromRequest(completeMessage);
+                    if(response !=  null)
+                    {
+                        callback(response[0], Types.response);
 
-            string requestSite = ReachSite(url);
-
-            if(requestSite != "")
-            {
-                buffer = Encoding.ASCII.GetBytes(ReachSite(url));
-                client.GetStream().Write(buffer, 0, buffer.Length);
-            } else
-            {
-                window.AddToLog("Requested site is not reachable.");
+                        WriteMessage(response[1]);
+                    }
+                }
             }
+            
             client.Close();
         }
 
-        private string ReachSite(string uri)
+        private string ReadMessage()
         {
-            HttpWebRequest webRequest = (HttpWebRequest)WebRequest.Create(uri);
-            webRequest.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+            string message = "";
+            do
+            {
+                int messageLength = ns.Read(buffer, 0, buffer.Length);
+                sb.AppendFormat("{0}", Encoding.UTF8.GetString(buffer, 0, messageLength));
+                message = sb.ToString();
+                
+            } while (client.GetStream().DataAvailable);
+
+            sb.Clear();
+            buffer = new byte[bufferSize];
+
+            return message;    
+        }
+
+        private void WriteMessage(string message)
+        {
+            if (message != null)
+            {
+                int messageSize = Encoding.UTF8.GetByteCount(message);
+                buffer = Encoding.UTF8.GetBytes(message);
+                ns.Write(buffer, 0, messageSize);
+                ns.Flush();
+            }
+            else
+            {
+                //Change to callback function.
+                window.AddToLog("Requested site is not reachable.");
+            }
+
+            buffer = new byte[bufferSize];
+        }
+
+        private string[] ReachSite(string url)
+        {
+            Uri uri = null;
+            Uri sysUri = null;
             try
             {
-                using (HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse())
-                using (Stream stream = response.GetResponseStream())
-                using (StreamReader reader = new StreamReader(stream))
+                if (url == "file:///E:/ProgrammingProjects/WIN/ProxyApp/ProxyApp/images/placeholder.png")
                 {
-                    return reader.ReadToEnd();
+                    sysUri = new Uri("E:\\ProgrammingProjects\\WIN\\ProxyApp\\ProxyApp\\images\\placeholder.png");
+                } else
+                {
+                    uri = new Uri(url);
+                }
+                try
+                {
+                    HttpWebRequest webRequest;
+                    if (sysUri != null)
+                    {
+                        var sysConverted = sysUri.LocalPath;
+                        //This can't be done. It's a FileRequest and not a HttpWebRequest.
+                        //TODO: This can't be done. It's a FileRequest and not a HttpWebRequest.
+                        webRequest = (HttpWebRequest)WebRequest.Create(sysConverted);
+                    } else
+                    {
+                        webRequest = (HttpWebRequest)WebRequest.Create(uri);
+                    }
+                    
+                    webRequest.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+                    try
+                    {
+                        using (HttpWebResponse response = (HttpWebResponse)webRequest.GetResponse())
+                        using (Stream stream = response.GetResponseStream())
+                        using (StreamReader reader = new StreamReader(stream))
+                        {
+                            foreach (string header in response.Headers)
+                                sb.AppendFormat("{0}:{1} \n", header, response.Headers[header]);
+                            sb.Append("\n");
+                            sb.Append("\n");
+
+                            string headers = sb.ToString();
+                            string message = reader.ReadToEnd();
+
+                            sb.Clear();
+
+                            return new string[] { headers, message };
+                        }
+                    }
+                    catch (Exception e) when (
+                      e is WebException ||
+                      e is InvalidOperationException ||
+                      e is NotSupportedException ||
+                      e is ProtocolViolationException
+                    )
+                    {
+                        string errorMsg = "An error has occured. \n" +
+                                "Error: " + e.Message + "\n" +
+                                "Url: " + uri + "\n";
+
+                        callback2(errorMsg, Types.response);
+                        
+                        Console.WriteLine("An error has occured: " + e.Message);
+                        return null;
+                    }
+                } catch(Exception e) when (
+                    e is NotSupportedException ||
+                    e is ArgumentNullException ||
+                    e is SecurityException
+                )
+                {
+                    Console.WriteLine("An error has occured: " + e.Message);
+                    return null;
                 }
             } catch(Exception e) when (
-                e is WebException ||
-                e is InvalidOperationException ||
-                e is NotSupportedException ||
-                e is ProtocolViolationException
-            )
-            {
-                window.AddToLog("An error has occured.");
+                e is ArgumentNullException ||
+                e is UriFormatException
+            ) {
                 Console.WriteLine("An error has occured: " + e.Message);
-                return "";
+                return null;
             }
-            
         }
 
         private string GetUrlFromRequest(string request)
         {
             string[] tokens = request.Split(' ');
-            return tokens[1];
+            if (tokens.Length > 1)
+            {
+                return tokens[1];
+            }
+            else return null;
+            
+        }
+
+        private bool RequestForImage(string request)
+        {
+            string[] headers = request.Split(' ');
+            foreach(string imageType in _imageExtensions)
+            {
+                if (headers[1].EndsWith(imageType))
+                {
+                    Console.WriteLine("Request for image made.");
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private string RemoveBrowserHeader(string request)
+        {
+            string[] headers = request.Split('\n');
+            foreach(string header in headers)
+            {
+                if(!header.StartsWith("User-Agent:"))
+                {
+                    Console.WriteLine(header);
+                    sb.Append(header);
+                }
+            }
+            string newMessage = sb.ToString();
+            sb.Clear();
+
+            return newMessage;
+        }
+
+        private string ChangeImageRequest(string request)
+        {
+            string[] headers = request.Split(' ');
+            string newUrl = "127.0.0.1:8080/images/placeholder.png";
+            headers[1] = newUrl;
+
+            return newUrl;
         }
     }
 }
